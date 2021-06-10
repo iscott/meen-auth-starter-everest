ce4ee8494bbad0f77d6a626ab53cd6bd3811a0bf
Ira Herman <ira@xynetik.com>
2021-06-10 14:28:59 -0700

    Completes part 1 - creating the routes and logic

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 // Database Configuration
 mongoose.connect(process.env.DATABASE_URL, {
@@ -23,10 +24,21 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // Body parser middleware: give us access to the req.body
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+	session({
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: false
+	})
+);
 
 // Routes / Controllers
 const userController = require('./controllers/users');
 app.use('/users', userController);
+
+const sessionsController = require('./controllers/sessions');
+app.use('/sessions', sessionsController);
+
 
 // Listener
 const PORT = process.env.PORT;
